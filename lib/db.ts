8c8dcc1f5badb9bacdbddd8a6647ecd6cd148918
Thiago <seuemail@dominio.com>
2026-01-31 17:06:57 -0300

    fix: ajuste conexão SQL para vercel

--- lib/db.ts
+++ lib/db.ts
@@ -1,26 +1,29 @@
 import sql from "mssql";
 
-const config: sql.config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER as string,
-  database: process.env.DB_DATABASE,
-  options: {
-    encrypt: false,
-    trustServerCertificate: true,
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    idleTimeoutMillis: 30000,
-  },
-};
-
 let pool: sql.ConnectionPool | null = null;
 
 export async function getDb() {
-  if (!pool) {
-    pool = await sql.connect(config);
+  if (pool) {
+    return pool;
   }
+
+  const config: sql.config = {
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    server: process.env.DB_SERVER!,
+    database: process.env.DB_NAME!,
+    options: {
+      encrypt: false,
+      trustServerCertificate: true,
+      enableArithAbort: true,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      idleTimeoutMillis: 30000,
+    },
+  };
+
+  pool = await sql.connect(config);
   return pool;
 }
